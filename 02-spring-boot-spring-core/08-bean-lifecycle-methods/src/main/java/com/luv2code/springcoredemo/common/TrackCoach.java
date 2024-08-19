faa761cff3a02048bd484699636bf550095a60bd
yasarcandamli <yasarcandamli@gmail.com>
2024-08-19 23:26:09 +0300

    Bean Lifecycle Methods completed.

---
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo/common/TrackCoach.java
@@ -0,0 +1,16 @@
+package com.luv2code.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrackCoach implements ICoach{
+
+    public TrackCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+}
---
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -0,0 +1,28 @@
+package com.luv2code.springcoredemo.rest;
+
+import com.luv2code.springcoredemo.common.ICoach;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    // define a private field for the dependency
+    private ICoach myCoach;
+
+    @Autowired
+    public DemoController(
+            @Qualifier("cricketCoach") ICoach theCoach) {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+        myCoach = theCoach;
+
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkOut() {
+        return myCoach.getDailyWorkout();
+    }
+}
