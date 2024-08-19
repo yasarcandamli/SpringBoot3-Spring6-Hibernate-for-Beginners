3646785d7266155ff75603f6b16cfd8889d1f61d
yasarcandamli <yasarcandamli@gmail.com>
2024-08-19 22:45:17 +0300

    Spring Boot 3 Quick Start section and Spring Core section completed until Bean Scope lesson

---
+++ 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/luv2code/springcoredemo/ICoach.java
@@ -0,0 +1,5 @@
+package com.luv2code.springcoredemo;
+
+public interface ICoach {
+    String getDailyWorkout();
+}
---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/luv2code/springcoredemo/common/ICoach.java
@@ -0,0 +1,5 @@
+package com.luv2code.springcoredemo.common;
+
+public interface ICoach {
+    String getDailyWorkout();
+}
