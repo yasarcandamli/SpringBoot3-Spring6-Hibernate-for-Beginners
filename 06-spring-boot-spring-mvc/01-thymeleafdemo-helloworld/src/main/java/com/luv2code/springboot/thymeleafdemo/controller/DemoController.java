ef1dc7e0b0eb3d276b1bd385360e6f1154dca68f
yasarcandamli <yasarcandamli@gmail.com>
2024-09-02 22:58:13 +0300

    Spring Boot - Spring MVC with Thymeleaf

---
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class DemoController {
+
+    // create a mapping for "/hello"
+    @GetMapping("/hello")
+    public String sayHello (Model theModel) {
+
+        theModel.addAttribute("theDate", java.time.LocalDateTime.now());
+
+        return "helloworld";
+    }
+}
