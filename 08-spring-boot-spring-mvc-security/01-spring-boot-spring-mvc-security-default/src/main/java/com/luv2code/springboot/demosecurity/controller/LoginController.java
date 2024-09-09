256843b8c75dc0a89095efb6ab458c9c22dcb182
yasarcandamli <yasarcandamli@gmail.com>
2024-09-09 18:59:55 +0300

    Spring MVC Security - Custom Access Denied Page

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/controller/LoginController.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/controller/LoginController.java
@@ -9,7 +9,14 @@ public class LoginController {
     @GetMapping("/showMyLoginPage")
     public String showMyLoginPage() {
 
-//        return "plain-login";
+        // return "plain-login";
         return "fancy-login";
+    }
+
+    // add request mapping for /access-denied
+    @GetMapping("/access-denied")
+    public String showAccessDenied() {
+
+        return "access-denied";
     }
 }
--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/luv2code/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -51,7 +51,9 @@ public class DemoSecurityConfig {
                                 .permitAll()
                 )
                 .logout(logout -> logout.permitAll()
-                );
+                )
+                .exceptionHandling(configurer ->
+                        configurer.accessDeniedPage("/access-denied"));
 
         return http.build();
     }
