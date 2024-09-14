d615368805175d0c558083e127821b28c9e7482a
yasarcandamli <yasarcandamli@gmail.com>
2024-09-14 22:49:43 +0300

    @OneToOneMapping - Coding - Run the Main App

--- 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
@@ -4,9 +4,10 @@ import com.luv2code.cruddemo.entity.Instructor;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-@Service
+@Repository
 public class AppDAOImpl implements AppDAO{
 
     // define field for entity manager
