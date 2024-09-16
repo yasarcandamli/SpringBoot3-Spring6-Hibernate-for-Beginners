38205b40a0b15c62b0e56465c6b4db3252cc5c78
yasarcandamli <yasarcandamli@gmail.com>
2024-09-16 23:11:18 +0300

    @OneToMany - Delete Course

--- 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/luv2code/cruddemo/dao/AppDAO.java
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/luv2code/cruddemo/dao/AppDAO.java
@@ -27,4 +27,6 @@ public interface AppDAO {
 
     void update(Course tempCourse);
 
+    void deleteCourseById(int theId);
+
 }
--- 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
@@ -117,4 +117,14 @@ public class AppDAOImpl implements AppDAO{
     public void update(Course tempCourse) {
         entityManager.merge(tempCourse);
     }
+
+    @Override
+    @Transactional
+    public void deleteCourseById(int theId) {
+        // retrieve the course
+        Course tempCourse = entityManager.find(Course.class, theId);
+
+        // delete the course
+        entityManager.remove(tempCourse);
+    }
 }
