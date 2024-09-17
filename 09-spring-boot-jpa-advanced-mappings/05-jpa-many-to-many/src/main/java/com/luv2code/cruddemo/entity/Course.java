7684986897fef35565be9f54936ef73c91c5f220
yasarcandamli <yasarcandamli@gmail.com>
2024-09-17 23:19:04 +0300

    @ManyToMany - Coding - Map Course to Student

--- 09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/luv2code/cruddemo/entity/Course.java
+++ 09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/luv2code/cruddemo/entity/Course.java
@@ -35,6 +35,15 @@ public class Course {
     @JoinColumn(name = "course_id")
     private List<Review> reviews;
 
+    @ManyToMany(fetch = FetchType.LAZY,
+                cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(
+            name = "course_student",
+            joinColumns = @JoinColumn(name = "course_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id")
+    )
+    private List<Student> students;
+
     public Course() {
     }
 
@@ -74,12 +83,28 @@ public class Course {
         this.reviews = reviews;
     }
 
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
     // add a convenience method
     public void addReview(Review theReview) {
         if (reviews == null) {
             reviews = new ArrayList<>();
         }
         reviews.add(theReview);
+    }
+
+    // add a convenience method
+    public void addStudent(Student theStudent) {
+        if (students == null) {
+            students = new ArrayList<>();
+        }
+        students.add(theStudent);
     }
 
     @Override
