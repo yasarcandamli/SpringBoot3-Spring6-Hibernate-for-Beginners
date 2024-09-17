854f07eac07db848c5f94f3e284340a584922251
yasarcandamli <yasarcandamli@gmail.com>
2024-09-17 22:08:01 +0300

    @OneToMany - Uni-Directional - Coding - Refactor Course Entity

--- 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/luv2code/cruddemo/entity/Course.java
+++ 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/luv2code/cruddemo/entity/Course.java
@@ -2,6 +2,9 @@ package com.luv2code.cruddemo.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "course")
 public class Course {
@@ -27,6 +30,10 @@ public class Course {
     @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     @JoinColumn(name = "instructor_id")
     private Instructor instructor;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "course_id")
+    private List<Review> reviews;
 
     public Course() {
     }
@@ -57,6 +64,22 @@ public class Course {
 
     public void setInstructor(Instructor instructor) {
         this.instructor = instructor;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    // add a convenience method
+    public void addReview(Review theReview) {
+        if (reviews == null) {
+            reviews = new ArrayList<>();
+        }
+        reviews.add(theReview);
     }
 
     @Override
