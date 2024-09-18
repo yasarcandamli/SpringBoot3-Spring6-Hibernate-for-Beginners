8e205fd0f49e75878b2e1507559701961075ad7d
yasarcandamli <yasarcandamli@gmail.com>
2024-09-18 12:50:46 +0300

    AOP - @Before Advice - Coding - Create Aspect

---
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,19 @@
+package com.luv2code.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+
+    // this is where we add all opf our related advices for logging
+
+    // let's start with an @Before advice
+
+    @Before("execution(public void addAcoount())")
+    public void beforeAddAccountAdvice() {
+        System.out.println("\n =====>>> Executing @Before advice on addAccount()");
+    }
+}
