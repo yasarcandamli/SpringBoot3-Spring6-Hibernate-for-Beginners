e26225d30077e37daa82c6448f38b054ba0981e6
yasarcandamli <yasarcandamli@gmail.com>
2024-09-18 16:38:03 +0300

    AOP - Combining Pointcuts

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -12,12 +12,24 @@ public class MyDemoLoggingAspect {
     @Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
     private void forDaoPackage() {}
 
-    @Before("forDaoPackage()")
+    // create a pointcut for getter methods
+    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
+    private void getter() {}
+
+    // create a pointcut for setter methods
+    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
+    private void setter() {}
+
+    // create a pointcut: include package ... exclude getter/setter
+    @Pointcut("forDaoPackage() && !(getter() || setter())")
+    private void forDaoPackageNoGetterSetter() {}
+
+    @Before("forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n =====>>> Executing @Before advice on addAccount()");
     }
 
-    @Before("forDaoPackage()")
+    @Before("forDaoPackageNoGetterSetter()")
     public void performApiAnalytics() {
         System.out.println("\n =====>>> Performing API analytics");
     }
--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/dao/AccountDAOImpl.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/dao/AccountDAOImpl.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class AccountDAOImpl implements AccountDAO{
+
+    private String name;
+    private String serviceCode;
+
     @Override
     public void addAccount(Account theAccount, boolean vipFlag) {
         System.out.println(getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
@@ -12,7 +16,31 @@ public class AccountDAOImpl implements AccountDAO{
 
     @Override
     public boolean doWork() {
-        System.out.println(getClass() + ": doWork()");
+        System.out.println(getClass() + ": in doWork()");
         return false;
+    }
+
+    @Override
+    public String getName() {
+        System.out.println(getClass() + ": in getName()");
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        System.out.println(getClass() + ": in setName()");
+        this.name = name;
+    }
+
+    @Override
+    public String getServiceCode() {
+        System.out.println(getClass() + ": in getServiceCode()");
+        return serviceCode;
+    }
+
+    @Override
+    public void setServiceCode(String serviceCode) {
+        System.out.println(getClass() + ": in setServiceCode()");
+        this.serviceCode = serviceCode;
     }
 }
