51d0eb212b20df166fd304b9bfab075fbb942d94
yasarcandamli <yasarcandamli@gmail.com>
2024-09-18 12:21:23 +0300

    AOP - @Before Advice - Coding - Create Target Object - AccountDAO

--- 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
@@ -1,5 +1,6 @@
 package com.luv2code.aopdemo;
 
+import com.luv2code.aopdemo.dao.AccountDAO;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +14,16 @@ public class AopdemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(AccountDAO theAccountDAO) {
 
 		return runner -> {
-			System.out.println("Hello World!");
+			demoTheBeforeAdvice(theAccountDAO);
 		};
+	}
+
+	private void demoTheBeforeAdvice(AccountDAO theAccountDAO) {
+
+		// call the business method
+		theAccountDAO.addAccount();
 	}
 }
