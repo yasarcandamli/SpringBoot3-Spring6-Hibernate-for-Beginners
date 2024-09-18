909107182162f1a1f6aff48e251ace5a02bbbfb0
yasarcandamli <yasarcandamli@gmail.com>
2024-09-18 15:26:58 +0300

    AOP - Pointcut Expressions - Coding - Match Method with Any Params

--- 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -12,7 +12,7 @@ public class MyDemoLoggingAspect {
 
     // let's start with a @Before advice
 
-    @Before("execution(* add*(com.luv2code.aopdemo.Account, ..))")
+    @Before("execution(* add*(..))")
     public void beforeAddAccountAdvice() {
         System.out.println("\n =====>>> Executing @Before advice on addAccount()");
     }
