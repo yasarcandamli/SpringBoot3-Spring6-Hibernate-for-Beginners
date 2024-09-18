1ba9b1814252bbde5a45dc993e55c61c5d434dd4
yasarcandamli <yasarcandamli@gmail.com>
2024-09-18 15:37:54 +0300

    AOP - Pointcut Expressions - Coding - Match Any Method in a Package

--- 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/dao/AccountDAOImpl.java
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/dao/AccountDAOImpl.java
@@ -9,4 +9,10 @@ public class AccountDAOImpl implements AccountDAO{
     public void addAccount(Account theAccount, boolean vipFlag) {
         System.out.println(getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
     }
+
+    @Override
+    public boolean doWork() {
+        System.out.println(getClass() + ": doWork()");
+        return false;
+    }
 }
--- 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/dao/MembershipDAOImpl.java
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/dao/MembershipDAOImpl.java
@@ -10,4 +10,9 @@ public class MembershipDAOImpl implements MembershipDAO{
 
         return true;
     }
+
+    @Override
+    public void goToSleep() {
+        System.out.println(getClass() + ": I'm going to sleep now");
+    }
 }
