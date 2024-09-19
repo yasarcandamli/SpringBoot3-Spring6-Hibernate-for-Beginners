e6561dd57f319e5bdf2715ed1ed560e3ad261611
yasarcandamli <yasarcandamli@gmail.com>
2024-09-19 21:23:10 +0300

    @AfterThrowing Advice

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -3,6 +3,7 @@ package com.luv2code.aopdemo.aspect;
 import com.luv2code.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.core.annotation.Order;
@@ -17,7 +18,20 @@ import java.util.List;
 @Order(2)
 public class MyDemoLoggingAspect {
 
-    // add a new advice for @AfterReturning on the findAccounts method
+    @AfterThrowing(
+            pointcut = "execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))",
+            throwing = "theExc"
+    )
+    public void afterThrowingFindAccountsAdvice(JoinPoint theJoinpoint, Throwable theExc) {
+
+        // print out which method we are advising on
+        String method = theJoinpoint.getSignature().toShortString();
+        System.out.println("\n ====>>> Executing @AfterThrowing on method: " + method);
+
+        // log the exception
+        System.out.println("\n ====>>> The exception is: " + theExc);
+    }
+
     @AfterReturning(
             pointcut = "execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))",
             returning = "result"
--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/dao/AccountDAO.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -9,6 +9,8 @@ public interface AccountDAO {
     // add a new method: findAccounts()
     List<Account> findAccounts();
 
+    List<Account> findAccounts(boolean tripWire);
+
     void addAccount(Account theAccount, boolean vipFlag);
 
     boolean doWork();
