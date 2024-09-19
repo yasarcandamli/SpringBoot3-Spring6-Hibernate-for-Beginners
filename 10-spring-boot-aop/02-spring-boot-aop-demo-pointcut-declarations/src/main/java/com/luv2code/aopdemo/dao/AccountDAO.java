0e9ad0b4f1449272ce8ba6f1bda2f8e8a2402d69
yasarcandamli <yasarcandamli@gmail.com>
2024-09-19 16:15:39 +0300

    @AfterReturning Advice

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/dao/AccountDAO.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -2,16 +2,23 @@ package com.luv2code.aopdemo.dao;
 
 import com.luv2code.aopdemo.Account;
 
+import java.util.List;
+
 public interface AccountDAO {
+
+    // add a new method: findAccounts()
+    List<Account> findAccounts();
+
     void addAccount(Account theAccount, boolean vipFlag);
 
     boolean doWork();
 
-    public String getName();
+    String getName();
 
-    public void setName(String name);
+    void setName(String name);
 
-    public String getServiceCode();
+    String getServiceCode();
 
-    public void setServiceCode(String serviceCode);
+    void setServiceCode(String serviceCode);
+
 }
